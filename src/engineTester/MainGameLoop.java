8a5725d83944bf96bd7f23d26bcaa1cdb3923549
aden <eleyaden@gmail.com>
2020-03-30 20:18:01 +0100

    Initial commit, basic rendering complete -main loop, renderer and shaders implemented. -Currently just renders a 2D rectangle using two triangles.

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,46 @@
+package engineTester;
+
+
+import org.lwjgl.opengl.Display;
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.RawModel;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+
+public class MainGameLoop {
+    public static void main(String[] args) {
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
+
+
+        float[] vertices = {
+                -0.5f,0.5f,0,   //V0
+                -0.5f,-0.5f,0,   //V1
+                0.5f,-0.5f,0,   //V2
+                0.5f,0.5f,0     //V3
+        };
+
+        int[] indices = {
+                0,1,3,  //Top left triangle (V0, V1, V3)
+                3,1,2   //Bottom left triangle (V3, V1, V2)
+        };
+
+        RawModel model = loader.loadToVao(vertices, indices);
+
+
+        while(!Display.isCloseRequested()) {
+            renderer.prepare();
+            shader.start();
+            renderer.render(model);
+            shader.stop();
+            DisplayManager.updateDisplay();
+        }
+
+        shader.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+    }
+}
