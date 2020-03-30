06deabcaaa83341fc23e517f7c8e5375a13f1a55
aden <eleyaden@gmail.com>
2020-03-30 22:52:25 +0100

    Added 4D matrix and maths -to allow for transformation of objects

--- src/shaders/StaticShader.java
+++ src/shaders/StaticShader.java
@@ -1,9 +1,13 @@
 package shaders;
 
+import org.lwjgl.util.vector.Matrix4f;
+
 public class StaticShader extends ShaderProgram {
 
     private static final String VERTEX_FILE = "src/shaders/vertexShader.txt";
     private static final String FRAGMENT_FILE = "src/shaders/fragmentShader.txt";
+
+    private int location_transformationMatrix;
 
 
     public StaticShader() {
@@ -12,8 +16,18 @@ public class StaticShader extends ShaderProgram {
 
 
     @Override
+    protected void getAllUniformLocations() {
+        location_transformationMatrix = super.getUniformLocation("transformationMatrix");
+    }
+
+    @Override
     protected void bindAttributes() {
         super.bindAttributes(0, "position");
         super.bindAttributes(1, "textureCoords");
     }
+
+    public void loadTransformationMatrix(Matrix4f matrix) {
+        super.loadMatrix(location_transformationMatrix, matrix);
+    }
+
 }
