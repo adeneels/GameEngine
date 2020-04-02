f30d7987abdccb3b5be100a346c51a2a403570a0
aden <eleyaden@gmail.com>
2020-04-02 14:36:13 +0100

    Terrain -Added terrain and rendered to the view

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -9,6 +9,7 @@ import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
 import renderEngine.*;
 import models.RawModel;
+import terrains.Terrain;
 import textures.ModelTexture;
 
 import java.util.ArrayList;
@@ -29,30 +30,31 @@ public class MainGameLoop {
         ModelTexture texture = bigDragon.getModelTexture();
         texture.setShineDamper(10);
         texture.setReflectivity(1);
+
+        Entity entity = new Entity(bigDragon, new Vector3f(0,0,-25),0,0,0,1);
+
+
         Light light = new Light(new Vector3f(3000,2000,3000), new Vector3f(1,1,1));
 
+        Terrain terrain = new Terrain(0, 0, loader, new ModelTexture(loader.loadTexture("grass")));
+        Terrain terrain2 = new Terrain(1, 0, loader, new ModelTexture(loader.loadTexture("white")));
 
-        List<Entity> toRender = new ArrayList<Entity>();
+
+
         Random random = new Random();
 
-        for (int i = 0; i < 5; i++) {
-            float x = random.nextFloat() * 100 - 50;
-            float y = random.nextFloat() * 100 - 50;
-            float z = random.nextFloat() * -300;
-            toRender.add(new Entity(bigDragon, new Vector3f(x, y , z),
-                    random.nextFloat() * 180f, random.nextFloat() * 180f,
-                    0f, 1f));
-        }
 
         Camera camera = new Camera();
-
+        camera.setYaw(180);
 
         MasterRenderer renderer = new MasterRenderer();
         while(!Display.isCloseRequested()) {
+
             camera.move();
-            for (Entity e : toRender) {
-                renderer.processEntity(e);
-            }
+            renderer.processEntity(entity);
+            renderer.processTerrain(terrain);
+            renderer.processTerrain(terrain2);
+
             renderer.render(light, camera);
             DisplayManager.updateDisplay();
         }
