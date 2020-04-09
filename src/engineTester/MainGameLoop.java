ef6a81bc33f9c13ca91ac847b81518b23fae14b4
adeneels <eleyaden@gmail.com>
2020-04-09 14:24:32 +0100

    player movement Added player movement and added player entity

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -4,6 +4,7 @@ package engineTester;
 import entities.Camera;
 import entities.Entity;
 import entities.Light;
+import entities.Player;
 import models.TexturedModel;
 import objConverter.ModelData;
 import objConverter.OBJFileLoader;
@@ -60,7 +61,7 @@ public class MainGameLoop {
         Terrain terrain2 = new Terrain(-1, -1, loader, texturePack, blendMap);
 
 
-        Camera camera = new Camera();
+        Camera camera = new Camera(100, 10 ,-100);
         camera.setYaw(180);
 
         List<Entity> entities = new ArrayList<Entity>();
@@ -71,7 +72,7 @@ public class MainGameLoop {
 
 
         Random random = new Random();
-        for (int i = 0; i < 500; i++) {
+        for (int i = 0; i < 200; i++) {
             if (i % 2 == 0) {
                 entities.add(new Entity(treeModel, new Vector3f(random.nextFloat() * 800 - 400, 0,
                         random.nextFloat() * -100), 0, 0 ,0, 0.5f));
@@ -81,10 +82,21 @@ public class MainGameLoop {
 
         }
 
+        ModelData bunnyData = OBJFileLoader.loadOBJ("bunny");
+        RawModel bunnyModel = loader.loadToVao(bunnyData.getVertices(), bunnyData.getTextureCoords(), bunnyData.getNormals(), bunnyData.getIndices());
+        TexturedModel bunnyTexture = new TexturedModel(bunnyModel, new ModelTexture(loader.loadTexture("mud")));
+
+        Player player = new Player(bunnyTexture, new Vector3f(100, 0 , -50), 0 , 0 , 0, 1);
+
+
+
         MasterRenderer renderer = new MasterRenderer();
         while(!Display.isCloseRequested()) {
 
             camera.move();
+            player.move();
+            renderer.processEntity(player);
+
 
             for (Entity e : entities) {
                 renderer.processEntity(e);
